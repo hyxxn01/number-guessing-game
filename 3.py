48015b2c7b6814358f6c2c860e583a66088d5999
hyxxn01 < >
2025-05-15 20:17:02 +0900

    mm

---
+++ 3.py
@@ -0,0 +1,20 @@
+import random 
+
+answer = random.randint(1, 100) 
+print("업다운 게임을 시작합니다! (1~100 사이 숫자를 맞혀보세요)") 
+
+while True:
+    guess = input("숫자를 입력하세요: ")
+
+    if not guess.isdigit():   
+        continue
+
+    guess = int(guess) #
+
+    if guess < answer:
+        print("UP!")
+    elif guess > answer:
+        print("DOWN!")
+    else:
+        print("정답입니다!")
+        break
